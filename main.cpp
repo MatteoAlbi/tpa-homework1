dcd061069ba0753685de5e2294cdb70bc2a366fa
MatteoAlbi <29534468+MatteoAlbi@users.noreply.github.com>
2021-07-09 11:10:13 +0200

    - Fixed quotes - Updated README

--- main.cpp
+++ main.cpp
@@ -17,16 +17,17 @@ int main(int argc, char ** argv) {
     double displacement = PI * pow(bore/2,2) * stroke * n;
     LBAMTTmotor * motor = LBAMTTinitMotor(n, bore, displacement, 90);
 
-    LBAMTTsaveToFile(LBAMTTmotorToStringSVG(motor, true, true), "motor_1.svg");
-    LBAMTTsetMotorAngle(motor, 630);
-    LBAMTTsaveToFile(LBAMTTmotorToStringSVG(motor, true, true), "motor_2.svg");
+    // LBAMTTsaveToFile(LBAMTTmotorToStringSVG(motor, true, true), "motor_1.svg");
+    // LBAMTTsetMotorAngle(motor, 630);
+    // LBAMTTsaveToFile(LBAMTTmotorToStringSVG(motor, true, true), "motor_2.svg");
+    
     //multiple test with angle from 0 a 330, step 15
-    // int n = 24
-    // for(int i=0;i<n;i++){
-    //     string s = "device_example" + to_string(i) + ".svg";
-    //     LBAMTTdeviceSetAngle(device, i*360/n);
-    //     LBAMTTsaveToFile(LBAMTTdeviceToStringSVG(device, 400, 200, true), s);
-    // }
+    int m = 24;
+    for(int i=0;i<m;i++){
+        string s = "motor_example" + to_string(i) + ".svg";
+        LBAMTTsetMotorAngle(motor, i*720/m);
+        LBAMTTsaveToFile(LBAMTTmotorToStringSVG(motor, true), s);
+    }
     LBAMTTdelete(motor);
 
     return 0;
